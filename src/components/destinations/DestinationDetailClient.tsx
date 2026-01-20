0b4ee9253879dbc46e040b1719233dda296568bb
jabir-h <jabir.h@wecodecommunity.com>
2026-01-20 10:49:13 +0530

    tripeloo updates

--- src/components/destinations/DestinationDetailClient.tsx
+++ src/components/destinations/DestinationDetailClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useCallback, useMemo } from "react";
+import { useEffect, useState, useCallback, useMemo, useRef } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { motion } from "framer-motion";
